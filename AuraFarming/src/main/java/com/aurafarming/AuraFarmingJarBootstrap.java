af075e8d472451683178a56e98fc0e02ebf88d55
kritak <kritak2081-0340@iimscollege.edu.np>
2026-03-17 18:39:02 +0545

    added final touch

---
+++ AuraFarming/src/main/java/com/aurafarming/AuraFarmingJarBootstrap.java
@@ -0,0 +1,10 @@
+package com.aurafarming;
+
+public final class AuraFarmingJarBootstrap {
+    private AuraFarmingJarBootstrap() {
+    }
+
+    public static void main(String[] args) {
+        AuraFarmingLauncher.main(args);
+    }
+}
